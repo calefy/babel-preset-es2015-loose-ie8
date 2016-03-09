42ffd57bb590bbbc957c7fe7aa3b35eb170b13c8
calefy <clfsw0201@gmail.com>
2016-03-09 11:26:15 +0800

    replace plugin es2015-classes

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ var LOOSE = {loose: true};
 
 module.exports = modify('es2015', {
     'transform-es2015-template-literals': LOOSE,
-    'transform-es2015-classes': LOOSE,
+    'transform-es2015-classes': false,
+    'transform-es2015-classes-ie8': LOOSE,
     'transform-es2015-computed-properties': LOOSE,
     'transform-es2015-for-of': LOOSE,
     'transform-es2015-spread': LOOSE,
